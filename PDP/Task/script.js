ed1d1064feb98d69255d2795d47234ff18402fb9
Sergey Klimuk <sergey_klimuk@mail.ru>
2024-03-10 22:21:55 +0300

    pdp task

---
+++ PDP/Task/script.js
@@ -0,0 +1,19 @@
+console.log('Script started');
+
+const divs = document.querySelectorAll('div');
+console.log('-----divs: ', divs);
+divs.forEach((elem) => {
+    this.addListener(elem)
+})
+
+
+function addListener(elem) {
+    elem.addEventListener('click', (event) => {
+        console.log('-----booble elemId: ', elem.id);
+        const innerElems = elem.querySelectorAll('#' + elem.id + ' > p')
+        console.log('-----innerElems: ', innerElems);
+    });
+    elem.addEventListener('click', (event) => {
+        console.log('-----capture elemId: ', elem.id);
+    }, {capture: true});
+}
