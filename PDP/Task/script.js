ee342badf13128a4a9745fcfc4b64b7cc2b756d2
Sergey Klimuk <sergey_klimuk@mail.ru>
2024-03-11 22:43:18 +0300

    pdp task

--- PDP/Task/script.js
+++ PDP/Task/script.js
@@ -1,19 +1,24 @@
 console.log('Script started');
 
-const divs = document.querySelectorAll('div');
-console.log('-----divs: ', divs);
-divs.forEach((elem) => {
-    this.addListener(elem)
-})
+// const divs = document.querySelectorAll('div');
+// console.log('-----divs: ', divs);
+// divs.forEach((elem) => {
+//     this.addListener(elem)
+// })
 
 
-function addListener(elem) {
-    elem.addEventListener('click', (event) => {
-        console.log('-----booble elemId: ', elem.id);
-        const innerElems = elem.querySelectorAll('#' + elem.id + ' > p')
-        console.log('-----innerElems: ', innerElems);
-    });
-    elem.addEventListener('click', (event) => {
-        console.log('-----capture elemId: ', elem.id);
-    }, {capture: true});
+// function addListener(elem) {
+//     elem.addEventListener('click', (event) => {
+//         // console.log('-----booble elemId: ', elem.id);
+//         // const innerElems = elem.querySelectorAll('#' + elem.id + ' > p')
+//         // console.log('-----innerElems: ', innerElems);
+//     });
+//     elem.addEventListener('click', (event) => {
+//         // console.log('-----capture elemId: ', elem.id);
+//     }, {capture: true});
+// }
+
+bbb.onclick = function() {
+    console.log('-----rrr.style.width: ', rrr.style.width);
+    rrr.style.width = rrr.style.width === '100%' ? '0%' : '100%'
 }
