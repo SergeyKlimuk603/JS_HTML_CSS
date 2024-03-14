85c13c29c56bc4e80152876e4dd88be978e8052f
Sergey Klimuk <sergey_klimuk@mail.ru>
2024-03-14 22:35:38 +0300

    pdp task

--- PDP/Task/script.js
+++ PDP/Task/script.js
@@ -1,24 +1,43 @@
 console.log('Script started');
+const runTimerIds = [];
 
-// const divs = document.querySelectorAll('div');
-// console.log('-----divs: ', divs);
-// divs.forEach((elem) => {
-//     this.addListener(elem)
-// })
+const divs = document.querySelectorAll('.divBlock');
+console.log('-----divs: ', divs);
+divs.forEach((elem) => {
+    this.addListener(elem)
+})
 
 
-// function addListener(elem) {
-//     elem.addEventListener('click', (event) => {
-//         // console.log('-----booble elemId: ', elem.id);
-//         // const innerElems = elem.querySelectorAll('#' + elem.id + ' > p')
-//         // console.log('-----innerElems: ', innerElems);
-//     });
-//     elem.addEventListener('click', (event) => {
-//         // console.log('-----capture elemId: ', elem.id);
-//     }, {capture: true});
-// }
+function addListener(elem) {
+    elem.addEventListener('myEvent', (event) => {
+        console.log('-----booble elemId: ', elem.id);
+        const innerElems = elem.querySelectorAll('#' + elem.id + ' > p')
+        //console.log('-----innerElems: ', innerElems);
+    });
+    elem.addEventListener('myEvent', (event) => {
+        console.log('-----capture elemId: ', elem.id);
+    }, {capture: true});
+    elem.addEventListener('animationend', (event) => {
+        console.log('-----animationend elemId: ', elem.id);
+        rrr.classList.remove('raise');
+    });
+}
 
 bbb.onclick = function() {
+    const myEvent = new Event('myEvent', {bubbles: true});
+    this.dispatchEvent(myEvent);
     console.log('-----rrr.style.width: ', rrr.style.width);
-    rrr.style.width = rrr.style.width === '100%' ? '0%' : '100%'
+    // rrr.classList.remove('raise');
+    rrr.classList.add('raise');
+    // rrr.style.width = rrr.style.width === '100%' ? '0%' : '100%'
+    // rrr.style.width = '0%';
+    
+}
+
+function clearAllTimers(timerIds) {
+    timerIds.array.forEach(timerId => {
+        clearTimeout(timerId);
+    });
+
+    return [];
 }
